cc69b7b7a15b65f8abdbbcc2a322751e0f3c4198
VladislavLut <143324701+VladislavLut@users.noreply.github.com>
2023-09-20 11:40:18 +0300

    Ex 1 was done

--- main.py
+++ main.py
@@ -4,16 +4,17 @@ y = float(input("Введіть друге число: "))
 z = float(input("Введіть третє число: "))
 
 
-operation = input("Виберіть операцію: 1 - сума, 2 - добуток: ")
+operation = input("Виберіть операцію: 1 - максимум, 2 - мінімум, 3 - середньоарифметичне: ")
 
 
 if operation == "1":
-    result = x + y + z
+    result = max(x, y, z)
 elif operation == "2":
-    result = x * y * z
+    result = min(x, y, z)
+elif operation == "3":
+    result = (x + y + z) / 3
 else:
     print("Неправильний вибір операції!")
-
 
 
 print("Результат:", result)
